aaa8a19012e56c9a48925c07ebb89fab513b39c5
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-11-07 20:17:35 +0700

    membuat class Mysplashscreen1

---
+++ lib/screen/splash_screen1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Mysplashscreen1 extends StatelessWidget {
+  const Mysplashscreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

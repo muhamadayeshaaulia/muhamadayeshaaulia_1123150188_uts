0708dc4edbaa1d8cdad6decc53d1b2d3345741cf
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-11-07 21:05:33 +0700

    text field untuk password

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -47,6 +47,25 @@ class MyLogin extends StatelessWidget {
               hintText: 'Masukkan Email Anda',
             ),
           ),
+           SizedBox(height: 20),
+
+          TextField(
+            keyboardType: TextInputType.visiblePassword,
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+              ),
+              prefixIcon: Icon(Icons.lock),
+              labelText: 'Password',
+              hintText: 'Masukkan Password Anda',
+              suffixIcon: IconButton(
+                icon: Icon(Icons.visibility),
+                onPressed: () {},
+              ),
+            ),
+          ),
         ],
       ),
     );

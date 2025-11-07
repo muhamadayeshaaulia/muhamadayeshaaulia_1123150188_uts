dab39ca4dbdaf93519c44382fc2e5196b942d7a6
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-11-07 20:27:26 +0700

    menambahkan gambar dan membuat container

--- lib/screen/splash_screen1.dart
+++ lib/screen/splash_screen1.dart
@@ -9,10 +9,21 @@ class Mysplashscreen1 extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Muhamad Ayesha Aulia",
-              style: TextStyle(
-                  fontSize: 10,
-                  color: Colors.green)),
+          Text(
+            "Muhamad Ayesha Aulia",
+            style: TextStyle(fontSize: 10, color: Colors.green),
+          ),
+          Container(
+            height: 240,
+            width: 240,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.green,
+              image: DecorationImage(
+                image: AssetImage("assets/images/gambar2.webp"),
+              ),
+            ),
+          ),
         ],
       ),
     );
